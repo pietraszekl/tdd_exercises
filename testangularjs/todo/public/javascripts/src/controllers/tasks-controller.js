7bdc94cf1d7f7ed7adf4bfaec17e11de2739344d
Lukasz Pietraszek <pietraszek@hotmail.co.uk>
2017-02-06 21:35:49 +0000

    Controller updateStatus and updateError

--- testangularjs/todo/public/javascripts/src/controllers/tasks-controller.js
+++ testangularjs/todo/public/javascripts/src/controllers/tasks-controller.js
@@ -1,6 +1,16 @@
-var TasksController = function () {
+var TasksController = function (tasksService) {
   var controller = this;
   controller.tasks = [];
+  controller.message = '';
+  controller.getTasks = function () {
+    tasksService.get(controller.updateTasks, controller.updateError);
+  };
+  controller.updateTasks = function (tasks) {
+    controller.tasks = tasks;
+  };
+  controller.updateError = function (error, status) {
+    controller.message = error + ' (status: ' + status + ')';
+  };
 };
 
-angular.module('todoapp').controller('TasksController', [TasksController])
+angular.module('todoapp').controller('TasksController', ['TasksService', TasksController]);
--- testangularjs/todo/test/client/tasks-controller-test.js
+++ testangularjs/todo/test/client/tasks-controller-test.js
@@ -1,15 +1,52 @@
 describe('tasks controller tests', function () {
+
+  var controller;
+  var tasksServiceMock;
+
+  beforeEach(module('todoapp'));
+  beforeEach(inject(function ($controller) {
+    tasksServiceMock = {};
+
+    controller = $controller('TasksController', {
+      TasksService: tasksServiceMock
+    });
+  }));
+
+
   it('should pass this canary test', function () {
     expect(true).to.be.true;
   });
 
-  var controller;
-  beforeEach(module('todoapp'));
-  beforeEach(inject(function ($controller) {
-    controller = $controller('TasksController');
-  }))
   it('tasks should be empty on create', function () {
     expect(controller.tasks).to.eql([]);
-  })
+  });
+
+  it('message should be empty on create', function () {
+    expect(controller.message).to.eql('');
+  });
+
+  it('getTasks should interact with the service', function (done) {
+    controller.updateTask = function () { };
+    controller.updateError = function () { };
+    tasksServiceMock.get = function (success, error) {
+      expect(success).to.be.eql(controller.updateTasks);
+      expect(error).to.be.eql(controller.updateError);
+      done();
+    };
+    controller.getTasks();
+  });
+
+  it('updateTasks should update tasks', function () {
+    var tasksStub = [{
+      sample: 1
+    }];
+    controller.updateTasks(tasksStub);
+    expect(controller.tasks).to.be.eql(tasksStub);
+  });
+
+  it('updateError should update message', function () {
+    controller.updateError('Not Found', 404);
+    expect(controller.message).to.be.eql('Not Found (status: 404)');
+  });
 });
 
