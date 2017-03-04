1c180ee4780cb38495bf7db2e7d87344b52cf20c
Lukasz Pietraszek <pietraszek@hotmail.co.uk>
2017-03-04 13:30:06 +0000

    getTasks and sortTasks

--- testangularjs/todo/public/javascripts/src/controllers/tasks-controller.js
+++ testangularjs/todo/public/javascripts/src/controllers/tasks-controller.js
@@ -1,4 +1,4 @@
-var TasksController = function (tasksService) {
+var TasksController = function (tasksService, $filter, $document) {
   var controller = this;
   controller.tasks = [];
   controller.message = '';
@@ -6,11 +6,16 @@ var TasksController = function (tasksService) {
     tasksService.get(controller.updateTasks, controller.updateError);
   };
   controller.updateTasks = function (tasks) {
-    controller.tasks = tasks;
+    controller.tasks = controller.sortTasks(tasks);
   };
   controller.updateError = function (error, status) {
     controller.message = error + ' (status: ' + status + ')';
   };
+  controller.sortTasks = function (tasks) {
+    var orderBy = $filter('orderBy');
+    return orderBy(tasks, ['year', 'month', 'day', 'name']);
+  };
+  $document.ready(controller.getTasks);
 };
 
-angular.module('todoapp').controller('TasksController', ['TasksService', TasksController]);
+angular.module('todoapp').controller('TasksController', ['TasksService', '$filter', '$document', TasksController]);
--- testangularjs/todo/test/client/tasks-controller-test.js
+++ testangularjs/todo/test/client/tasks-controller-test.js
@@ -1,12 +1,14 @@
 describe('tasks controller tests', function () {
 
   var controller;
-  var tasksServiceMock;
+  var tasksServiceMock = {};
+  var documentReadyHandler;
 
   beforeEach(module('todoapp'));
-  beforeEach(inject(function ($controller) {
-    tasksServiceMock = {};
-
+  beforeEach(inject(function ($controller, $document) {
+    $document.ready = function (handler) {
+      documentReadyHandler = handler;
+    }
     controller = $controller('TasksController', {
       TasksService: tasksServiceMock
     });
@@ -47,6 +49,45 @@ describe('tasks controller tests', function () {
   it('updateError should update message', function () {
     controller.updateError('Not Found', 404);
     expect(controller.message).to.be.eql('Not Found (status: 404)');
+  });
+  it('sortTasks should sort based on year', function () {
+    var task1 = { name: 'task a', month: 1, day: 10, year: 2017 };
+    var task2 = { name: 'task b', month: 5, day: 28, year: 2016 };
+    var sorted = controller.sortTasks([task1, task2])
+    expect(sorted).to.be.eql([task2, task1]);
+  });
+  it('sortTasks should by year, then by month', function () {
+    var task1 = { name: 'task a', month: 2, day: 10, year: 2017 };
+    var task2 = { name: 'task b', month: 1, day: 10, year: 2016 };
+    var task3 = { name: 'task c', month: 1, day: 10, year: 2017 };
+    var sorted = controller.sortTasks([task1, task2, task3])
+    expect(sorted).to.be.eql([task2, task3, task1]);
+  });
+  it('sortTasks should by year, month then day', function () {
+    var task1 = { name: 'task a', month: 1, day: 20, year: 2017 };
+    var task2 = { name: 'task b', month: 1, day: 14, year: 2017 };
+    var task3 = { name: 'task c', month: 1, day: 9, year: 2017 };
+    var sorted = controller.sortTasks([task1, task2, task3])
+    expect(sorted).to.be.eql([task3, task2, task1]);
+  });
+  it('sortTasks should by year, month, day then name', function () {
+    var task1 = { name: 'task a', month: 1, day: 14, year: 2017 };
+    var task2 = { name: 'task b', month: 1, day: 14, year: 2017 };
+    var task3 = { name: 'task c', month: 1, day: 14, year: 2017 };
+    var sorted = controller.sortTasks([task1, task2, task3])
+    expect(sorted).to.be.eql([task1, task2, task3]);
+  });
+  it('updateTask should call sortTasks', function () {
+    var tasksStub = [{ sample: 1 }];
+    controller.sortTasks = function (tasks) {
+      expect(tasks).to.be.eql(tasksStub);
+      return '..sorted..';
+    };
+    controller.updateTasks(tasksStub);
+    expect(controller.tasks).to.be.eql('..sorted..');
+  });
+  it('should register getTasks as handler for document ready', function () {
+    expect(documentReadyHandler).to.be.eql(controller.getTasks);
   });
 });
 
