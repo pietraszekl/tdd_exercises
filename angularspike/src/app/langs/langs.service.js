34c8948723d863c627e8094447c0695ea7b3d353
Lukasz Pietraszek <pietraszek@hotmail.co.uk>
2017-03-19 21:53:59 +0000

    service

--- angularspike/src/app/langs/langs.service.js
+++ angularspike/src/app/langs/langs.service.js
@@ -8,6 +8,15 @@
         return this.http.get('/languages')
           .map(this.extractData)
           .catch(this.returnError);
+      },
+      extractData: function (response) {
+        if (response !== 200) {
+          throw new Error("error getting data, status: " + response.status);
+        }
+        return response.text();
+      },
+      returnError: function (error) {
+        return Rx.Obsrvable.throw(error.message || "error, status: " + error.status);
       }
     });
 })(window.app || (window.app = {}));
